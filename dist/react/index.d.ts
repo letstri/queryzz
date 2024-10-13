04979661434895ff52a92d7ff7ccf9e2c43f56b4
Valerii Strilets <valerii.strilets@gmail.com>
2024-10-14 00:57:19 +0300

    fix build

--- dist/react/index.d.ts
+++ dist/react/index.d.ts
@@ -1,6 +1,10 @@
 import * as react from 'react';
 import { V as Value } from '../shared/queryzz.c9af6ff2.js';
 
-declare function useQuery(key: string): [string | number | boolean | Value[] | null | undefined, react.Dispatch<react.SetStateAction<string | number | boolean | Value[] | null | undefined>>];
+interface Options {
+    parse?: boolean;
+    array?: boolean;
+}
+declare function useQuery<T extends Value>(key: string, options?: Options): [T | undefined, react.Dispatch<react.SetStateAction<T | undefined>>];
 
 export { useQuery };
--- dist/react/index.mjs
+++ dist/react/index.mjs
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
-import { g as getQuery, s as setQuery } from '../shared/queryzz.cfd2bd28.mjs';
+import { s as setQuery, g as getQuery } from '../shared/queryzz.cfd2bd28.mjs';
 
-function useQuery(key) {
-  const queryState = useState(getQuery()[key]);
+function useQuery(key, options) {
+  const q = () => getQuery({
+    parse: options?.parse,
+    arrays: options?.array ? [key] : []
+  });
+  const queryState = useState(q()[key]);
   useEffect(() => {
-    const currentQuery = getQuery()[key];
+    const currentQuery = q()[key];
     if (currentQuery !== queryState[0]) {
       setQuery({ [key]: queryState[0] });
     }
   }, [key, queryState]);
   useEffect(() => {
     const listener = () => {
-      const currentQuery = getQuery()[key];
+      const currentQuery = q()[key];
       if (currentQuery !== queryState[0]) {
         queryState[1](currentQuery);
       }
